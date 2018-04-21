2a1c8c8ecdc240bf64de0d5f0627780854971656
kmooney <kmooney@gmail.com>
2018-04-21 16:44:33 +0000

    still playin'

--- src/three/static/js/entities.js
+++ src/three/static/js/entities.js
@@ -4,18 +4,22 @@ window.Entities = {
         var highlight = new THREE.MeshBasicMaterial({color: "#f0f000"})
         this.position = new THREE.Vector3(0,0,0);
         this.axisAngle = new THREE.Vector3(0,0,1);
-        this.direction = -Math.PI / 2 ;
-        this.velocity = 0.05;
+        this.direction = Math.PI * 3 ;
+        this.velocity = 0.09;
         this.kart = kartScene;
 
         this.update = function(dt) {
             self.kart.position.z += this.velocity * Math.cos(this.direction);
             self.kart.position.x += this.velocity * Math.sin(this.direction);
             self.kart.rotation.z = this.direction;
+
+            if (self.velocity >= 0)
+                self.velocity -= 0.01;
+
         }
     }
 }
 
 window.Instances = {
-
+    
 };
