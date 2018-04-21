dfde19af1364d1320f7ea5bb7c1cf6491105a2ba
kmooney <kmooney@gmail.com>
2018-04-21 20:46:15 +0000

    i'm happy with the kart now

--- src/three/static/js/entities.js
+++ src/three/static/js/entities.js
@@ -1,82 +1,85 @@
 var UP = new THREE.Vector3(0,1,0);
-    
-
 window.Entities = {
-    Player: function(kartScene) {
+    Player: function(kart) {
         var self = this;
         var highlight = new THREE.MeshBasicMaterial({color: "#f0f000"})
-        this.position = new THREE.Vector3(0,0,0);
-        this.axisAngle = new THREE.Vector3(0,0,1);
-        this.direction = 0;
-        this.engine = new THREE.Vector3(0,0,0.03);
-        this.velocity = new THREE.Vector3(0,0,0);
-        this.friction = new THREE.Vector3(0,0,0);
-        this.kart = kartScene;
+        var friction = new THREE.Vector3(0,0,1);
+        var turnAngle = 0.07;
+
+        var velocity = new THREE.Vector3(0,0,0);
+        self.velocity = velocity;
+        var direction = 0;
+        
+        
+        this.kart = kart;
         this.obj3D = new THREE.Object3D();
         this.obj3D.add(this.kart);
-        this.gas = false;
-
-        window.addEventListener('keydown', function(e) {
         
-            if (e.keyCode == 38) {
-                self.gas = true;
-            }
-            if (e.keyCode == 40) {
-                self.brake = true;
-            }
-            if (e.keyCode == 37) {
-                self.left = true;
-            }
-            if (e.keyCode == 39) {
-                self.right = true;
-            }
-        });
-    
-        window.addEventListener('keyup', function(e) {
-             if (e.keyCode == 38) {
-                self.gas = false;
-            }
-            if (e.keyCode == 40) {
-                self.brake = false;
-            }
-            if (e.keyCode == 37) {
-                self.left = false;
-            }
-            if (e.keyCode == 39) {
-                self.right = false;
-            }
-        });
+        var gas = false, brake = false, left = false, right = false;
 
-        this.update = function(dt,dungeon) {
-            if(self.left){
-                self.direction += 0.1;
+        this.update = function(dt, dungeon) {            
+            if(left && velocity.length() > 0.01){
+                direction += turnAngle;
+                velocity.applyAxisAngle(UP, turnAngle);
             }
-            if(self.right){
-                self.direction -= 0.1;
-            }   
-            var v = new THREE.Vector3(0,0,0);
-            // add current velocity
-            v.add(this.velocity);
-            v.multiplyScalar(0.8);
-
-            // add engine power
-            var thrust = new THREE.Vector3(0,0,0);
-            if( v.length() < 0.03 && this.gas ){
-                thrust.add(self.engine);
-                thrust.applyAxisAngle(UP,this.direction);
-                v.add(thrust);
+            if(right && velocity.length() > 0.01){
+                direction -= turnAngle;
+                velocity.applyAxisAngle(UP, -turnAngle);                
+            }
+            if(gas) {
+                var t = new THREE.Vector3(0,0,0.01);
+                t.applyAxisAngle(UP, direction);   
+                velocity.add(t);
+            }
+            if(brake) {
+                velocity.multiplyScalar(0.1);
             }
 
-            self.obj3D.position.add(v);
-            self.kart.rotation.z = this.direction;
+            velocity.multiplyScalar(0.95);
 
-            this.velocity = v;
+            velocity.clampLength(0,0.1);
+            
             dungeon.applyBounds(self);
+
+            this.obj3D.position.add(velocity);
+            kart.rotation.z = direction;
+            
         }
         
         this.addToScene = function(scene){
             scene.add(this.obj3D);
         }
+
+        window.addEventListener('keydown', function(e) {
+        
+            if (e.keyCode == 38) {
+                gas = true;
+            }
+            if (e.keyCode == 40) {
+                brake = true;
+            }
+            if (e.keyCode == 37) {
+                left = true;
+            }
+            if (e.keyCode == 39) {
+                right = true;
+            }
+        });
+    
+        window.addEventListener('keyup', function(e) {
+            if (e.keyCode == 38) {
+                gas = false;
+            }
+            if (e.keyCode == 40) {
+                brake = false;
+            }
+            if (e.keyCode == 37) {
+                left = false;
+            }
+            if (e.keyCode == 39) {
+                right = false;
+            }
+        });
     }
 }
 
